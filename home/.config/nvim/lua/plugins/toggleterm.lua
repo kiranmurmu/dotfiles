2b4e484c18dbc3f472fd321d5aa6a14c54b1d19f
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2025-07-19 06:16:48 +0530

    Refactor toggleterm in neovim config

--- home/.config/nvim/lua/plugins/toggleterm.lua
+++ home/.config/nvim/lua/plugins/toggleterm.lua
@@ -5,55 +5,69 @@ return {
     -- event = "VeryLazy",
     opts = {
         open_mapping = false,
-        size = function(term)
-            if term.direction == "horizontal" then
-                return vim.o.lines * 0.5
-            elseif term.direction == "vertical" then
-                return vim.o.columns * 0.5
-            end
-        end,
-        dir = "%:p:h",
         shade_terminals = true,
         insert_mappings = false,
         hidden = true,
-        -- disable highlights (darken terminal)
         highlights = {
             border = "Normal",
             background = "Normal",
         },
-        display_name = " Terminal ",
         direction = "float",
         float_opts = {
             border = "curved",
             title_pos = "center",
         },
-        on_create = function(term)
-            vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-\\><C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
-            vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-\\><C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
-            vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<Esc>", "<cmd>close<CR>", { noremap = true, silent = true })
-            vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-c>", "<cmd>close<CR>", { noremap = true, silent = true })
-        end,
-        on_open = function(term)
-            local dir = vim.fn.expand("#:p:h")
-            if term.dir == dir then return end
-            term:send({ string.format("cd %s", dir)})
-            term.dir = dir
-        end,
     },
+    terminals = function(self)
+        local width = math.ceil(math.min(vim.o.columns, math.max(80, vim.o.columns * 0.8 - 4)))
+        local height = math.ceil(math.min(vim.o.lines, math.max(20, vim.o.lines * 0.8 - 1)))
+        local row = math.ceil(vim.o.lines - height) * 0.5 - 2
+        local col = math.ceil(vim.o.columns - width) * 0.5 - 1
+        local terms = {}
+
+        terms.default = vim.tbl_deep_extend("keep", self.opts, {
+            display_name = " Terminal ",
+            cmd = "bash",
+            dir = "%:p:h",
+            float_opts = {
+                width = width,
+                height = height,
+                row = row,
+                col = col,
+            },
+            on_create = function(term)
+                vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-\\><C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
+                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-\\><C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
+                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<Esc>", "<cmd>close<CR>", { noremap = true, silent = true })
+                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-c>", "<cmd>close<CR>", { noremap = true, silent = true })
+            end,
+            on_open = function(term)
+                local dir = vim.fn.expand("#:p:h")
+                if term.dir == dir then return end
+                term:send({ string.format("cd %s", dir)})
+                term.dir = dir
+            end,
+        })
+
+        return terms
+    end,
     keys = function(self)
-        local terminals = {}
-        local toggleterm = require("toggleterm.terminal").Terminal:new(self.opts)
-        -- Toggle terminal window
+        local Terminal = require("toggleterm.terminal").Terminal
+        local terminal = self:terminals()
+        local default = Terminal:new(terminal.default)
         function terminals.toggleterm()
+
+        function terminal.toggle_default()
             if vim.api.nvim_get_mode().mode == "nt" then
                 vim.cmd("startinsert")
             else
-                toggleterm:toggle()
+                default:toggle()
             end
         end
         -- Return keymaps
+
         return {
-            { "<C-\\><C-\\>", mode = { "n", "v", "i" }, terminals.toggleterm, desc = "Toggle Terminal window" },
+            { "<C-\\><C-\\>", mode = { "n", "v", "i" }, terminal.toggle_default, desc = "Toggle Terminal window" },
         }
     end,
 }
