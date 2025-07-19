7a276c020752eebde07809e88bc783b22f0dadb8
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2025-07-19 22:57:41 +0530

    Fix floating window colors in neovim themes

--- home/.config/nvim/lua/plugins/themes.lua
+++ home/.config/nvim/lua/plugins/themes.lua
@@ -30,6 +30,17 @@ return colorscheme("kanagawa", {
                 colors = {
                     theme = { all = { ui = { bg_gutter = "none" } } },
                 },
+                overrides = function(colors)
+                    local theme = colors.theme
+                    return {
+                        NormalFloat = { bg = "none" },
+                        FloatBorder = { bg = "none" },
+                        FloatTitle = { bg = "none" },
+                        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+                        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                    }
+                end,
             },
         },
     },
