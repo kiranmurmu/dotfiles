b89bbcea103041ab99e2f24bd9c9e3c5dc5cc1d8
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2026-02-04 07:45:59 +0530

    Fix keymap not working for 'Toggle Terminal window' in neovim

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -2,16 +2,6 @@
 vim.keymap.set({ "n", "x" }, "<Space>", "<Nop>", { silent = true, desc = "Remove default keybind" })
 -- Clear search highlights when pressing <Esc> in normal mode
 vim.keymap.set({ "n" }, "<Esc>", "<cmd>nohlsearch<CR>", { silent = true, desc = "Clear search highlights" })
--- Enter terminal (t) mode when vim mode is normal terminal (nt)
-vim.keymap.set(
-    { "n" }, "<C-\\><C-\\>",
-    function()
-        if vim.api.nvim_get_mode().mode == "nt" then
-            vim.cmd("startinsert")
-        end
-    end,
-    { silent = true, desc = "Enter terminal mode" }
-)
 -- Exit terminal mode, use <C-S-\><C-n> key to force exit terminal mode
 vim.keymap.set({ "t" }, "<C-\\><C-\\>", "<C-\\><C-n>", { silent = true, desc = "Exit terminal mode" })
 vim.keymap.set({ "n" }, "<leader>bc", "<cmd>close<CR>", { silent = true, desc = "Close current buffer" })
