ea6930fd566f2a133bae6f136943130a6dc8cd78
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2026-02-05 15:29:44 +0530

    Remove lsp-hover and diagnostic config from lspconfig.lua file

--- home/.config/nvim/lua/plugins/lspconfig.lua
+++ home/.config/nvim/lua/plugins/lspconfig.lua
@@ -36,35 +36,6 @@ return {
             vim.lsp.config(server, config)
             vim.lsp.enable(server)
         end
-        -- Diagnostics config
-        vim.diagnostic.config({
-            -- virtual_text = true,
-            -- virtual_lines = true,
-            underline = true,
-            severity_sort = true,
-            float = {
-                border = "rounded",
-                source = true,
-                max_width = 100,
-            },
-            signs = {
-                text = {
-                    [vim.diagnostic.severity.ERROR] = "󰅚",
-                    [vim.diagnostic.severity.WARN] = "󰀪",
-                    [vim.diagnostic.severity.INFO] = "󰋽",
-                    [vim.diagnostic.severity.HINT] = "󰌶",
-                }
-            },
-        })
-        -- Hover config
-        local hover = vim.lsp.buf.hover
-        ---@diagnostic disable-next-line: duplicate-set-field
-        vim.lsp.buf.hover = function()
-            return hover({
-                border = "single",
-                max_width = math.floor(vim.o.columns * 0.7),
-                max_height = math.floor(vim.o.lines * 0.7),
-            })
-        end
     end
+    -- WARNING: Do not add 'keys' property in this config, otherwise lsp will not load!
 }
