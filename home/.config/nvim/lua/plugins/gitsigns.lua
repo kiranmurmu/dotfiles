e290f0ec1b9821d79c22650c329c8f03410b0e02
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2026-02-08 10:58:04 +0530

    Change keymaps for gitsigns neovim plugin

--- home/.config/nvim/lua/plugins/gitsigns.lua
+++ home/.config/nvim/lua/plugins/gitsigns.lua
@@ -38,14 +38,14 @@ return {
             { "]c", mode = { "n" }, methods.nav_next, desc = "Gitsigns: next hunk" },
             { "[c", mode = { "n" }, methods.nav_prev, desc = "Gitsigns: previous hunk" },
             { "ih", mode = { "o", "x" }, gitsigns.select_hunk, desc = "Gitsigns: select hunk" },
-            { "<C-h>s", mode = { "n" }, gitsigns.stage_hunk, desc = "Gitsigns: stage hunk" },
-            { "<C-h>r", mode = { "n" }, gitsigns.reset_hunk, desc = "Gitsigns: reset hunk" },
-            { "<C-h>s", mode = { "v" }, methods.stage_hunk, desc = "Gitsigns: stage hunk" },
-            { "<C-h>r", mode = { "v" }, methods.reset_hunk, desc = "Gitsigns: reset hunk" },
-            { "<C-h>o", mode = { "n" }, gitsigns.preview_hunk, desc = "Gitsigns: preview hunk open" },
-            { "<C-h>i", mode = { "n" }, gitsigns.preview_hunk_inline, desc = "Gitsigns: preview hunk inline" },
-            { "<C-h>bs", mode = { "n" }, gitsigns.stage_buffer, desc = "Gitsigns: buffer stage" },
-            { "<C-h>br", mode = { "n" }, gitsigns.reset_buffer, desc = "Gitsigns: buffer reset" },
+            { "<C-g>s", mode = { "n" }, gitsigns.stage_hunk, desc = "Gitsigns: stage hunk" },
+            { "<C-g>r", mode = { "n" }, gitsigns.reset_hunk, desc = "Gitsigns: reset hunk" },
+            { "<C-g>s", mode = { "v" }, methods.stage_hunk, desc = "Gitsigns: stage hunk" },
+            { "<C-g>r", mode = { "v" }, methods.reset_hunk, desc = "Gitsigns: reset hunk" },
+            { "<C-g>k", mode = { "n" }, gitsigns.preview_hunk, desc = "Gitsigns: preview hunk in-hover" },
+            { "<C-g>i", mode = { "n" }, gitsigns.preview_hunk_inline, desc = "Gitsigns: preview hunk in-line" },
+            { "<C-g>bs", mode = { "n" }, gitsigns.stage_buffer, desc = "Gitsigns: buffer stage" },
+            { "<C-g>br", mode = { "n" }, gitsigns.reset_buffer, desc = "Gitsigns: buffer reset" },
         }
     end
 }
