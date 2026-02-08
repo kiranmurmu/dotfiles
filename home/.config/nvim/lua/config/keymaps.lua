1831bfe684eaa5c33c7f5a1cede733dcfe289cc5
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2026-02-08 12:00:47 +0530

    Remove keymap for buffer delete/close in keymaps neovim config

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -4,8 +4,6 @@ vim.keymap.set({ "n", "x" }, "<Space>", "<Nop>", { silent = true, desc = "Remove
 vim.keymap.set({ "n" }, "<Esc>", "<cmd>nohlsearch<CR>", { silent = true, desc = "Clear search highlights" })
 -- Exit terminal mode, use <C-S-\><C-n> key to force exit terminal mode
 vim.keymap.set({ "t" }, "<C-\\><C-\\>", "<C-\\><C-n>", { silent = true, desc = "Exit terminal mode" })
-vim.keymap.set({ "n" }, "<leader>bc", "<cmd>close<CR>", { silent = true, desc = "Close current buffer" })
-vim.keymap.set({ "n" }, "<leader>bd", "<cmd>bdelete<CR>", { silent = true, desc = "Delete current buffer" })
 -- Custom keymaps for insert mode
 vim.keymap.set({ "i" }, "<C-h>", "<Left>", { silent = true, desc = "<CTRL-h> to <Left> in insert mode" })
 vim.keymap.set({ "i" }, "<C-l>", "<Right>", { silent = true, desc = "<CTRL-l> to <Right> in insert mode" })
