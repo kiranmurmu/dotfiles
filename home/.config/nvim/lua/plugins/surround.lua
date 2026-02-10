5362ef9ce6fe6e59c0381a18ffaf041488ff920c
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2026-02-10 21:11:54 +0530

    Change keymap for visual mode in surround neovim plugin

--- home/.config/nvim/lua/plugins/surround.lua
+++ home/.config/nvim/lua/plugins/surround.lua
@@ -1,3 +1,4 @@
+-- lua/plugins/surround.lua
 return {
     "kylechui/nvim-surround",
     version = "^3.0.0",
@@ -10,7 +11,7 @@ return {
             normal_cur = "yss",
             normal_line = "yS",
             normal_cur_line = "ySS",
-            visual = "S",
+            visual = "gs",
             visual_line = "gS",
             delete = "ds",
             change = "cs",
