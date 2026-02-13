09a64d7853b7c20823ef2aad385f2b49ba41ef05
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2026-02-13 12:16:04 +0530

    Refactor terminal neovim config file

--- home/.config/nvim/lua/config/terminal.lua
+++ home/.config/nvim/lua/config/terminal.lua
@@ -1,15 +1,13 @@
+local augroup = vim.api.nvim_create_augroup("UserConfigs", { clear = true })
+
 -- Configure terminal to start in insert mode by default
-local augroup = vim.api.nvim_create_augroup("user.terminal.config", { clear = false })
-
-local function start_in_insert()
-    local mode = vim.api.nvim_get_mode().mode
-    if mode:match("nt") or not mode:match("t") then -- 'nt' is normal mode and 't' is insert mode in the terminal
-        vim.schedule(function() vim.cmd("startinsert") end)
-    end
-end
-
 vim.api.nvim_create_autocmd({ "TermOpen", "BufWinEnter" }, {
     pattern = { "term://*" },
     group = augroup,
-    callback = start_in_insert,
+    callback = function()
+        local mode = vim.api.nvim_get_mode().mode
+        if mode:match("nt") or not mode:match("t") then -- 'nt' is normal mode and 't' is insert mode in the terminal
+            vim.schedule(function() vim.cmd("startinsert") end)
+        end
+    end,
 })
