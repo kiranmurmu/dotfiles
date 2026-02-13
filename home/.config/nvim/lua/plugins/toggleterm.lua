b18be40e67987141a193be38de19f4563de3d128
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2026-02-13 15:42:38 +0530

    Refactor toggleterm neovim plugin

--- home/.config/nvim/lua/plugins/toggleterm.lua
+++ home/.config/nvim/lua/plugins/toggleterm.lua
@@ -2,70 +2,80 @@
 return {
     "akinsho/toggleterm.nvim",
     version = "*",
-    -- event = "VeryLazy",
-    opts = {
-        open_mapping = false,
-        shade_terminals = true,
-        insert_mappings = false,
-        hidden = true,
-        highlights = {
-            border = "Normal",
-            background = "Normal",
-        },
-        direction = "float",
-        float_opts = {
-            border = "single",
-            title_pos = "center",
-        },
-    },
-    terminals = function(self)
+    opts = function()
         local width = math.ceil(math.min(vim.o.columns, math.max(80, vim.o.columns * 0.8 - 4)))
         local height = math.ceil(math.min(vim.o.lines, math.max(20, vim.o.lines * 0.8 - 2)))
         local row = math.ceil(vim.o.lines - height) * 0.5 - 1
         local col = math.ceil(vim.o.columns - width) * 0.5 - 1
-        local terms = {}
 
-        terms.default = vim.tbl_deep_extend("keep", self.opts, {
-            display_name = " Terminal ",
-            cmd = "bash",
-            dir = "%:p:h",
+        return {
+            open_mapping = false,
+            shade_terminals = true,
+            insert_mappings = false,
+            hidden = true,
+            highlights = {
+                border = "Normal",
+                background = "Normal",
+            },
+            direction = "float",
             float_opts = {
+                border = "single",
+                title_pos = "center",
                 width = width,
                 height = height,
                 row = row,
                 col = col,
             },
-            on_create = function(term)
-                vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-\\><C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
-                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-\\><C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
-                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<Esc>", "<cmd>close<CR>", { noremap = true, silent = true })
-                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-c>", "<cmd>close<CR>", { noremap = true, silent = true })
-            end,
-            on_open = function(term)
-                local dir = vim.fn.expand("#:p:h")
-                if term.dir == dir then return end
-                term:send({ string.format("cd %s", dir)})
-                term.dir = dir
-            end,
-        })
-
-        return terms
+            display_name = " Terminal ",
+            dir = "%:p:h",
+        }
     end,
+    terminal = {
+        new = function(self, config)
+            config = config or {}
+
+            self.opts = self.opts or {
+                bash = vim.tbl_deep_extend("keep", config, {
+                    cmd = "bash",
+                    on_create = function(term)
+                        vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<C-\\><C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
+                        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-\\><C-\\>", "<cmd>close<CR>", { noremap = true, silent = true })
+                        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<Esc>", "<cmd>close<CR>", { noremap = true, silent = true })
+                        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<C-c>", "<cmd>close<CR>", { noremap = true, silent = true })
+                    end,
+                    on_open = function(term)
+                        local dir = vim.fn.expand("#:p:h")
+                        if term.dir == dir then return end
+                        term:send({ string.format("cd %s", dir)})
+                        term.dir = dir
+                    end,
+                }),
+                default = config,
+            }
+
+            return self.opts
+        end,
+    },
     keys = function(self)
         local Terminal = require("toggleterm.terminal").Terminal
-        local terminal = self:terminals()
-        local default = Terminal:new(terminal.default)
+        local opts = self.opts() or {}
+        local term = self.terminal:new(opts)
 
-        function terminal.toggle_default()
-            if vim.api.nvim_get_mode().mode == "nt" then
-                vim.cmd("startinsert")
-            else
-                default:toggle()
+        term.api = {}
+        do
+            local bash = Terminal:new(term.bash)
+
+            function term.api.toggle_bash()
+                if vim.api.nvim_get_mode().mode == "nt" then
+                    vim.cmd("startinsert")
+                else
+                    bash:toggle()
+                end
             end
         end
 
         return {
-            { "<C-\\><C-\\>", mode = { "n", "v", "i" }, terminal.toggle_default, desc = "Toggle Terminal window" },
+            { "<C-\\><C-\\>", mode = { "n", "v", "i" }, term.api.toggle_bash, desc = "Toggle terminal window" },
         }
     end,
 }
