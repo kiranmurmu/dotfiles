8d9576595dba9816f6c8bb429e2a7e73ede92a8e
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2026-02-13 12:18:54 +0530

    Add autocommand to properly close 'Man pages' with q key in neovim

--- home/.config/nvim/lua/config/terminal.lua
+++ home/.config/nvim/lua/config/terminal.lua
@@ -11,3 +11,12 @@ vim.api.nvim_create_autocmd({ "TermOpen", "BufWinEnter" }, {
         end
     end,
 })
+
+-- Properly close Man pages with 'q' key
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "man" },
+  group = augroup,
+  callback = function(self)
+      vim.api.nvim_buf_set_keymap(self.buf, "n", "q", "<cmd>bdelete<CR>", { silent = true })
+  end,
+})
