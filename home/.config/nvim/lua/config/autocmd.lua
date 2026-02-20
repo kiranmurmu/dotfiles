081ba77548aa235d5cb8685f979c4d0221a7369b
Kiran Murmu <44278642+kiranmurmu@users.noreply.github.com>
2026-02-20 13:15:46 +0530

    Configure terminal only keymaps in autocmd neovim config

--- home/.config/nvim/lua/config/autocmd.lua
+++ home/.config/nvim/lua/config/autocmd.lua
@@ -1,14 +1,11 @@
-local augroup = vim.api.nvim_create_augroup("UserConfigs", { clear = true })
+local augroup = vim.api.nvim_create_augroup("UserConfigs", { clear = false })
 
--- Configure terminal to start in insert mode by default
-vim.api.nvim_create_autocmd({ "TermOpen", "BufWinEnter" }, {
+-- Configure terminal only keymaps
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
     pattern = { "term://*" },
     group = augroup,
-    callback = function()
-        local mode = vim.api.nvim_get_mode().mode
-        if mode:match("nt") or not mode:match("t") then -- 'nt' is normal mode and 't' is insert mode in the terminal
-            vim.schedule(function() vim.cmd("startinsert") end)
-        end
+    callback = function(self)
+      vim.api.nvim_buf_set_keymap(self.buf, "n", "<C-\\><C-\\>", "<cmd>startinsert<CR>", { silent = true })
     end,
 })
 
